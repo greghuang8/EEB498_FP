9807306969d9d9d7c303e9375ea313e85c3404f0
Greg Huang <gregoryhuang2005@gmail.com>
2018-03-20 02:02:36 -0400

    script to calculate beta diversities for updated zones

---
+++ beta_diversity.R
@@ -0,0 +1,85 @@
+# beta_diversity.R
+#
+# Calculate the beta-diversity for each zones 
+# After the zones have been reclassified into four separate sections by pop.
+# density (prepped by zone_reclassify.R and excel), it is now time to run 
+# beta-diversity calculations in each of those zones. The code will first
+# run beta-diversity in the general cases (e.g. updated_zone1_all.csv), 
+# then potentially for the more refined, yearly zone csv files.
+# The beta diversities will encompass the hosts and parasites. 
+# 
+# Version: 1.2
+# Author: Greg Huang
+# Last update: March 20, 2018
+#
+# Versions: 
+#         1.1  Quick write up of the code
+#         1.2  Clean up; Apply code to actual data
+# ==============================================================================
+
+#### Install required packages ####
+if(!require(vegan, quietly = TRUE)){
+  install.packages("vegan")
+  library(vegan)
+}
+
+# package adespatial contains beta.div,
+# the tool I'll use to get the beta diversity calculations
+if(!require(adespatial, quietly = TRUE)){
+  install.packages("adespatial")
+  library(adespatial)
+}
+
+#### Load in .csv files ####
+order_counts <- read.csv("zones_orders_counts_key.csv")
+colnames(order_counts)[1] <- "Bats"
+
+infection_counts <- read.csv("zones_infections_counts_key.csv")
+colnames(infection_counts)[1] <- "Bacteria"
+
+
+#### Beta diversity calculation ####
+
+# LCBD - Local Contribution to Beta Diversity
+# SCBD - Species Contribution to Beta Diversity
+
+# Calculate beta-diversities for orders across zones
+order_counts_hel <- decostand(order_counts, "hel") # Hellinger transform
+(beta_r1_order <- beta.div(order_counts))  
+beta_r1_order<-(beta_r1_order = beta.div(order_counts))
+
+# Calculate beta-diversities for infections across zones
+infection_counts_hel <- decostand(infection_counts, "hel")
+(beta_r1_infections <- beta.div(infection_counts))  
+beta_r1_infections<-(beta_r1_infections = beta.div(infection_counts))
+
+# Save results into data frames
+order_SCBD <- data.frame(beta_r1_order$SCBD)
+order_LCBD <- data.frame(beta_r1_order$LCBD)
+infections_SCBD <- data.frame(beta_r1_infections$SCBD)
+infections_LCBD <- data.frame(beta_r1_infections$LCBD)
+
+
+#### Check for significance ####
+
+# count the locations of p.LCBD values that has significance (p) <= 0.05
+(order_r1_sig_LCBD <- which(beta_r1_order$p.LCBD <= 0.05))
+# not good: it's all pretty high, return null
+(infection_r1_sig_LCBD <- which(beta_r1_infections$p.LCBD <= 0.05))
+
+
+#### Maps of LCBD values and richness per quadrat ####
+# Leave out for now: twc.xy unavailable 
+# plot(twc.xy, asp=1, type="n", xlab="Longitude (m)", ylab="Latitude (m)", main="LCBD indices, microphytophagous mites", xlim=c(0,2.5), ylim=c(0,10))
+# points(twc.xy,pch=21, col="white", bg="steelblue2", cex=18*sqrt(beta.res1$LCBD))
+# points(twc.xy[res1.signif.LCBD,],pch=21, col="white", bg="red", cex=18*sqrt(beta.res1$LCBD[res1.signif.LCBD]))
+# text(twc.xy, labels=1:70, pos=4, cex=0.8, offset=0.7)
+
+
+#### INAKI - SCBD Plots ####
+
+plot(sort(beta_r1_order$SCBD,decreasing=T),type="n")
+text(sort(beta_r1_order$SCBD,decreasing=T),labels=names(sort(beta_r1_order$SCBD,decreasing=T)),cex=0.5)
+
+plot(sort(beta_r1_infections$SCBD,decreasing=T),type="n")
+text(sort(beta_r1_infections$SCBD,decreasing=T),labels=names(sort(beta_r1_infections$SCBD,decreasing=T)),cex=0.5)
